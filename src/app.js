6d9521e2c256a19f4c948a0fd79dcbb67f6a16d1
Mohd Faiz Reza <mdfaizraza502@gmail.com>
2025-04-29 17:38:31 +0530

    Add utils files

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express, { application } from 'express';
+import cors from "cors"
+import cookiParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ['GET','POST','PUT','DELET'],
+    allowedHeaders:['content-type', 'Authorization']
+}))
+
+app.use(express.json({limit: '16kb'}))
+app.use(express.urlencoded({extended: true, limit: '16kb'}))
+app.use(express.static("public"))
+app.use(cookiParser)
+
+
+
+
+
+
+export { app }
